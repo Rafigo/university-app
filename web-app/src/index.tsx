09d2ee4545e28511ea70e20da9abacb7359d8616
rpoublan <raphaelpoublan@gmail.com>
2021-11-26 22:48:00 +0100

    [FRONT][CONFIG] Update app name in package.json file

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import './styles/index.scss';
-
-/* <Auth0Provider
-    domain="dev-ez3iwtzi.eu.auth0.com"
-    clientId="HTTS6VQujf9UaCjMuvfSBwwWAaGGkwNI"
-    redirectUri={window.location.origin}
-  > </Auth0Provider> */
 
 ReactDOM.render(
   <React.StrictMode>
